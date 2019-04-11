28129010a8345d2f396e56f9593c92950542f58a
Corey Clemente <coreycle1@gmail.com>
2019-04-10 23:18:41 -0400

    blah

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,5 +1,5 @@
 // load county data
-var counties = {};
+const counties = {};
 d3.csv('/data/county.csv', (data) => {
   for (let row of data) {
     while (row.FIPS.length < 5) row.FIPS = '0' + row.FIPS;
@@ -7,25 +7,89 @@ d3.csv('/data/county.csv', (data) => {
   }
 });
 
-// create map
-var svgMap = d3.select('.county-map');
-var mapPath = d3.geoPath();
-d3.json('https://d3js.org/us-10m.v1.json', (error, us) => {
-  if (error) throw error;
+function createMap() {
+  var svgMap = d3.select('.county-map');
+  var mapPath = d3.geoPath();
+  d3.json('/data/us-10m.v1.json', (error, us) => {
+    if (error) throw error;
 
-  svgMap.append('g')
-    .attr('class', 'counties')
-    .selectAll('path')
-    .data(topojson.feature(us, us.objects.counties).features)
-    .enter().append('path')
-    .attr('d', mapPath)
-    .on('mouseover', (d) => {
-      let loc = counties[d.id].County + ', ' + counties[d.id].State;
-      console.log(loc);
-      document.getElementById('my-text').innerHTML = loc;
-    });
+    // alphabetize counties for easer sorted rendering later
+    let d = us.objects.counties.geometries;
+    d.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0));
+    us.objects.counties.geometries = d;
 
-    svgMap.append('path')
-    .attr('class', 'county-borders')
-    .attr('d', mapPath(topojson.mesh(us, us.objects.counties, function (a, b) { return a !== b; })));
-});
+    // create the county paths
+    svgMap
+      .append('g')
+      .attr('class', 'counties')
+      .selectAll('path')
+      .data(topojson.feature(us, us.objects.counties).features)
+      .enter()
+      .append('path')
+      .attr('d', mapPath);
+
+    // create the county borders
+    svgMap
+      .append('path')
+      .attr('class', 'county-borders')
+      .attr('d', mapPath(topojson.mesh(us, us.objects.counties, (a, b) => { return a !== b; })));
+
+    // create each circle per county
+    svgMap
+      .append('g')
+      .attr('class', 'circles')
+      .selectAll('circle')
+      .data(topojson.feature(us, us.objects.counties).features)
+      .enter()
+      .append('circle')
+      .attr('cx', (d) => {
+        let points = d.geometry.coordinates[0];
+        while (typeof points[0][0] !== 'number') points = points[0];
+        let C_x = 0;
+        let A = 0;
+        for (let i in points.slice(0, points.length - 1)) {
+          i = parseInt(i);
+          let p1 = points[i];
+          let p2 = points[i + 1];
+          if (p2 === points[0]) break;
+          A += (p1[0] * p2[1] - p2[0] * p1[1]);
+        }
+        A *= 0.5;
+        for (let i in points.slice(0, points.length - 1)) {
+          i = parseInt(i);
+          let p1 = points[i];
+          let p2 = points[i + 1];
+          if (p2 === points[0]) break;
+          C_x += ((p1[0] + p2[0]) * (p1[0] * p2[1] - p2[0] * p1[1]));
+        }
+        C_x *= (1 / (6 * A));
+        return C_x;
+      })
+      .attr('cy', (d) => {
+        let points = d.geometry.coordinates[0];
+        while (typeof points[0][0] !== 'number') points = points[0];
+        let C_y = 0;
+        let A = 0;
+        for (let i in points.slice(0, points.length - 1)) {
+          i = parseInt(i);
+          let p1 = points[i];
+          let p2 = points[i + 1];
+          if (p2 === points[0]) break;
+          A += (p1[0] * p2[1] - p2[0] * p1[1]);
+        }
+        A *= 0.5;
+        for (let i in points.slice(0, points.length - 1)) {
+          i = parseInt(i);
+          let p1 = points[i];
+          let p2 = points[i + 1];
+          if (p2 === points[0]) break;
+          C_y += ((p1[1] + p2[1]) * (p1[0] * p2[1] - p2[0] * p1[1]));
+        }
+        C_y *= (1 / (6 * A));
+        return C_y;
+      })
+      .attr('r', 2);
+  });
+}
+
+createMap();
