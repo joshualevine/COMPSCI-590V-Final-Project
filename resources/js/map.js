53799d96793039a2952df65fb40090e2c2111dd6
Corey Clemente <coreycle1@gmail.com>
2019-04-10 20:37:06 -0400

    working map county names showing up! yay

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,17 +1,31 @@
-var svg = d3.select(".county-map");
-var path = d3.geoPath();
+// load county data
+var counties = {};
+d3.csv('/data/county.csv', (data) => {
+  for (let row of data) {
+    while (row.FIPS.length < 5) row.FIPS = '0' + row.FIPS;
+    counties[row.FIPS] = row;
+  }
+});
 
+// create map
+var svgMap = d3.select('.county-map');
+var mapPath = d3.geoPath();
 d3.json('https://d3js.org/us-10m.v1.json', (error, us) => {
   if (error) throw error;
 
-  svg.append("g")
-    .attr("class", "counties")
-    .selectAll("path")
+  svgMap.append('g')
+    .attr('class', 'counties')
+    .selectAll('path')
     .data(topojson.feature(us, us.objects.counties).features)
-    .enter().append("path")
-    .attr("d", path);
+    .enter().append('path')
+    .attr('d', mapPath)
+    .on('mouseover', (d) => {
+      let loc = counties[d.id].County + ', ' + counties[d.id].State;
+      console.log(loc);
+      document.getElementById('my-text').innerHTML = loc;
+    });
 
-  svg.append("path")
-    .attr("class", "county-borders")
-    .attr("d", path(topojson.mesh(us, us.objects.counties, function (a, b) { return a !== b; })));
+    svgMap.append('path')
+    .attr('class', 'county-borders')
+    .attr('d', mapPath(topojson.mesh(us, us.objects.counties, function (a, b) { return a !== b; })));
 });
